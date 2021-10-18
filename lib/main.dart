c9230c1579163ebabae7309be3d923728403d19e
aqilat94 <aqilat94@gmail.com>
2021-10-18 17:12:16 +0800

    discard body to other class

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'dart:math';
 
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
+import 'package:iring/components/body_landing.dart';
 
 void main() => runApp(
       MaterialApp(
@@ -17,123 +18,7 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 60),
-            Text.rich(
-              TextSpan(
-                text: 'Good Day!',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 40,
-                    color: Colors.purple.shade600),
-              ),
-            ),
-            SizedBox(height: 20),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 5),
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                height: 54,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                          offset: Offset(0, 5),
-                          blurRadius: 10,
-                          color: Colors.black.withOpacity(0.20))
-                    ]),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Search for hospitals & clinics",
-                    hintStyle: TextStyle(color: Colors.black26),
-                    enabledBorder: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    icon: Icon(
-                      Icons.search,
-                      color: Colors.purple.shade600,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Upcoming Appointment',
-              style: TextStyle(
-                fontSize: 13,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey.shade600,
-              ),
-            ),
-            SizedBox(height: 10),
-            Card(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15)),
-              color: Colors.red.shade400,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  ListTile(
-                    leading: Icon(
-                      Icons.apartment_outlined,
-                      color: Colors.white,
-                    ),
-                    title: Text(
-                      'Hospital Pengajar UnisZA',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                    subtitle: Text(
-                      '10 October 2021  9:00 am',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                    trailing: Icon(
-                      Icons.arrow_right,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'IRING Panel Facilities',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.grey.shade600,
-                    fontSize: 13,
-                  ),
-                ),
-                Row(
-                  children: [
-                    Text(
-                      'See more',
-                      style: TextStyle(
-                        fontSize: 10,
-                      ),
-                    ),
-                    Icon(Icons.arrow_right),
-                  ],
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+      body: BodyLanding(),
     );
   }
 }
